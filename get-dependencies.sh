991190f4c8b900b65485c1ee91a7a09ef1b99633
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-13 10:15:24 -0400

    install packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm kicad kicad-library
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
