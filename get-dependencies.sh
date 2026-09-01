43417c184286e6801f93be11889d5a3f8dc3b09f
xlcdev <119954023+xlc-dev@users.noreply.github.com>
2026-09-01 22:38:19 +0200

    feat: bundle KiCad 3D model library (#18)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm kicad kicad-library protobuf
+pacman -Syu --noconfirm kicad kicad-library kicad-library-3d protobuf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
